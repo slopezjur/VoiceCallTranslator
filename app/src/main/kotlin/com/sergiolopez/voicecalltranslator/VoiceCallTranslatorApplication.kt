b02c665b394310b3737ff71973d2708b72439ae4
Sergio López <sergiolopezjurado@gmail.com>
2024-04-18 23:34:11 +0200

    remove firebase initializer from application

--- app/src/main/kotlin/com/sergiolopez/voicecalltranslator/VoiceCallTranslatorApplication.kt
+++ app/src/main/kotlin/com/sergiolopez/voicecalltranslator/VoiceCallTranslatorApplication.kt
@@ -1,21 +1,18 @@
 package com.sergiolopez.voicecalltranslator
 
 import android.app.Application
-import com.sergiolopez.voicecalltranslator.feature.common.data.mapper.UserMapper
-import com.sergiolopez.voicecalltranslator.feature.splash.Initializer
-import com.sergiolopez.voicecalltranslator.feature.splash.InitializerManager
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
 class VoiceCallTranslatorApplication : Application() {
 
-    @Inject
-    lateinit var initializerManager: InitializerManager
+    /*@Inject
+    lateinit var initializerManager: InitializerManager*/
 
     override fun onCreate() {
         super.onCreate()
 
-        initializerManager.init(this)
+        //initializerManager.init(this)
     }
 }
